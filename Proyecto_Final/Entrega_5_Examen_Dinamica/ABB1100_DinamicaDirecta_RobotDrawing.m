281cc3b0dc3227c44708a0cc4ec2b276dae5967c
Luis Fernando Cuevas Cuauhtle <luisfernandocuevas@outlook.com>
2022-06-07 15:20:08 -0500

    Animaciones Examen

--- Proyecto_Final/Entrega_5_Examen_Dinamica/ABB1100_DinamicaDirecta_RobotDrawing.m
+++ Proyecto_Final/Entrega_5_Examen_Dinamica/ABB1100_DinamicaDirecta_RobotDrawing.m
@@ -1,14 +1,5 @@
 %% Comportamiento Obtenido
 
-%{
-sim_q1 = out.q_sim.Data(:,1);
-sim_q2 = out.q_sim.Data(:,2);
-sim_q3 = out.q_sim.Data(:,3);
-sim_q4 = out.q_sim.Data(:,4);
-sim_q5 = out.q_sim.Data(:,5);
-sim_q6 = out.q_sim.Data(:,6);
-%}
-
 sim_q1 = out.q_sim.Data(:,1);
 sim_q2 = out.q_sim.Data(:,2);
 sim_q3 = out.q_sim.Data(:,3);
@@ -28,6 +19,6 @@ sim_Time = out.q_sim.Time;
 for i = 1:1:(length(sim_Time))
     clf
     ABB1100_RobotDrawing(q1s(i),q2s(i),q3s(i),q4s(i),q5s(i),q6s(i))
-    pause(0.0001)
-    %exportgraphics(gcf,'ABB1100_Simulacion_Dinamica_5F_-45.gif','Append',true);
+    pause(0.01)
+    %exportgraphics(gcf,'ABB1100_Simulacion_Dinamica_0F_P0.gif','Append',true);
 end
